734f2a26d7aec78de1bff29598986edd681ea5cb
sinelmo <49057487+sinelmo@users.noreply.github.com>
2022-11-07 22:29:00 +0900

    외계어 사전

--- 1.java
+++ 1.java
@@ -32,3 +32,38 @@ class Solution {
         return false;
     }
 }
+
+// ----------------------------------굳이 StirngBuilder을 쓸 필요가 없었다.-----------------------------------------------------
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+        StringBuilder alien = new StringBuilder();
+        
+        for(int i = 0; i < dic.length; i++) {
+            if(alienDic(spell, dic[i])) {
+                answer = 1;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public boolean alienDic(String[] spell, String dic) {
+        String alien = dic;
+        
+        for(int i = 0; i < spell.length; i++) {
+            if(alien.indexOf(spell[i]) >= 0) {
+                alien = alien.replace(spell[i], "");
+            } else {
+                return false;
+            }
+        }
+        
+        if(alien.length() == 0) {
+            return true;
+        }
+        
+        return false;
+    }
+}
+

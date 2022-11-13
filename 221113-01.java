feb619a9e68078f73563808fed9f730a04c1fa1b
sinelmo <49057487+sinelmo@users.noreply.github.com>
2022-11-13 17:43:46 +0900

    최빈값 구하기 (못풀었음) : 0이 포함되어 있을 경우 추가

--- 221113-01.java
+++ 221113-01.java
@@ -6,34 +6,30 @@ class Solution {
         int answer = 0;
         int max = 0;
         ArrayList<Integer> list = new ArrayList<>();
-        
+
         Arrays.sort(array);
-        
-        // list에 array에 있는 숫자 중복제거하여 넣기
+
         for(int i : array) {
             if(!list.contains(i)) {
                 list.add(i);
             }
         }
         // System.out.println(list);
-        
-        // 나온 횟수 넣을 배열
+
         int[] count = new int[list.size()];
-        
-        // list에 들어있는 숫자 위치와 같은 [j]번째에 count 값 넣기
+
         for(int i : array) {
             for(int j = 0; j < count.length; j++) {
                 if(i == list.get(j)) {
                     count[j]++;
-                    // System.out.println("count[" + j + "] : " + count[j]);
+                     //System.out.println("count[" + j + "] : " + count[j]);
                 }
             }
         }
-        
-        // count에서 최댓값 구하기
+
         max = count[0];
         if(array.length != 1) {
-            for(int i = 0; i < count.length; i++) {
+            for(int i = 1; i < count.length; i++) {
                 if(max < count[i]) {
                     // System.out.println("max : " + max + " 보다 count[" + i + "] : " + count[i] + " 가 더 큽니다.");
                     max = count[i];
@@ -46,7 +42,7 @@ class Solution {
         } else {
             answer = array[0];
         }
-        
+
         return answer;
     }
 }   

16b3e6a3d751a750e08252f718862485513c0f55
sinelmo <49057487+sinelmo@users.noreply.github.com>
2022-11-17 23:55:05 +0900

    중앙값 구하기

---
+++ 221117-02.java
@@ -0,0 +1,14 @@
+// 중앙값 구하기
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        Arrays.sort(array);
+        
+        answer = array[array.length / 2];
+        
+        return answer;
+    }
+}

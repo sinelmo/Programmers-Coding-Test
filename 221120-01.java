424dfb243494e961d3e5a67586a2f684d16aecd5
sinelmo <49057487+sinelmo@users.noreply.github.com>
2022-11-20 22:38:11 +0900

    옷가게 할인 받기

---
+++ 221120-01.java
@@ -0,0 +1,18 @@
+// 옷가게 할인 받기
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        
+        if(price >= 100000 && price < 300000) {
+            answer = (int)(price * 0.95);
+        } else if(price >= 300000 && price < 500000) {
+            answer = (int)(price * 0.9);
+        } else if(price >= 500000) {
+            answer = (int)(price * 0.8);
+        } else {
+            answer = price;
+        }
+        
+        return answer;
+    }
+}

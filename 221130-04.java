dbe5a6b33c1c7bfe98566202bc0301ac44fcce0f
sinelmo <49057487+sinelmo@users.noreply.github.com>
2022-11-30 23:11:04 +0900

    문자열 계산하기 ( 테스트case = "2 + 2 - 5 + 2 - 1

---
+++ 221130-04.java
@@ -0,0 +1,22 @@
+// 문자열 계산하기
+import java.math.BigInteger;
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        String result[] = my_string.split(" ");
+        
+        for(int i = 0; i < result.length; i++) {
+            if(i == 0) {
+                answer = Integer.parseInt(result[i]);
+            } else if(result[i].equals("+")) {
+                answer += Integer.parseInt(result[i + 1]);
+            } else if(result[i].equals("-")) {
+                answer -= Integer.parseInt(result[i + 1]);
+            }
+        }
+        
+        return answer;
+    }
+}

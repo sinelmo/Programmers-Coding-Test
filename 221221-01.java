3de6e46316866f147c03b64ed538755aa1112d1d
sinelmo <49057487+sinelmo@users.noreply.github.com>
2022-12-21 22:49:56 +0900

    짝수와 홀수

---
+++ 221221-01.java
@@ -0,0 +1,12 @@
+// 짝수와 홀수
+class Solution {
+    public String solution(int num) {
+        String answer = "Odd";
+        
+        if(num % 2 == 0) {
+            answer = "Even";
+        }
+        
+        return answer;
+    }
+}

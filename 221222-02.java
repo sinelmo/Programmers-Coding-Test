fc9a851f9858b59f1d5045fd3ba15f87e70e4fcd
sinelmo <49057487+sinelmo@users.noreply.github.com>
2022-12-22 23:13:05 +0900

    문자열 내 p와 y의 개수

---
+++ 221222-02.java
@@ -0,0 +1,21 @@
+// 문자열 내 p와 y의 개수
+class Solution {
+    boolean solution(String s) {
+        int p = 0;
+        int y = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+                p++;
+            } else if(s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
+                y++;
+            }
+        }
+        
+        if(p == y) {
+            return true;
+        } 
+
+        return false;
+    }
+}

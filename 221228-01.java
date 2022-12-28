34b22f5644138013e12564ed4ad0decb46ad417b
sinelmo <49057487+sinelmo@users.noreply.github.com>
2022-12-28 23:26:58 +0900

    콜라츠 추측

---
+++ 221228-01.java
@@ -0,0 +1,22 @@
+// 콜라츠 추측
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        
+        for(int i = 0; i < 500; i++) {
+            if(num == 1) {
+                break;
+            }
+            
+            if(num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = num * 3 + 1;
+            }
+            
+            answer++;
+        }
+        
+        return (num == 1) ? answer : -1;
+    }
+}

75492967aafdd009301cfdd4c349e5bb49a65f37
sinelmo <49057487+sinelmo@users.noreply.github.com>
2022-12-30 21:50:23 +0900

    음양 더하기

---
+++ 221230-01.java
@@ -0,0 +1,16 @@
+// 음양 더하기
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i = 0; i < absolutes.length; i++) {
+            if(!signs[i]) {
+                absolutes[i] *= -1;
+            }
+            
+            answer += absolutes[i];
+        }
+        
+        return answer;
+    }
+}

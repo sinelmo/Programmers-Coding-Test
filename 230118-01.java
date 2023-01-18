7512a5f14fb6944585d9dd34c8ef499b62294793
sinelmo <49057487+sinelmo@users.noreply.github.com>
2023-01-18 23:33:42 +0900

    [1차] 비밀지도
    런타임 에러가 날때는 int 형을 long 으로 바꿔보자

--- 230118-01.java
+++ 230118-01.java
@@ -1,14 +1,11 @@
-// [1차] 비밀지도 (못풀엇음)
+// [1차] 비밀지도
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
         
         for(int i = 0; i < arr1.length; i++) {
-            System.out.println(String.format("%0" + n + "d", Integer.parseInt(Integer.toString(arr1[i], 2))));
-        }
-        System.out.println();
-        for(int i = 0; i < arr2.length; i++) {
-            System.out.println(String.format("%0" + n + "d", Integer.parseInt(Integer.toString(arr2[i], 2))));
+            answer[i] = String.format("%0" + n + "d", Long.parseLong(Long.toString(arr1[i] | arr2[i], 2)))
+                                                                                .replace("1", "#").replace("0", " ");
         }
         
         return answer;

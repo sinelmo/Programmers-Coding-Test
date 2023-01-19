ddf24f53ece65e2c1a5f66392764e07a2c6a82fb
sinelmo <49057487+sinelmo@users.noreply.github.com>
2023-01-19 23:49:28 +0900

    삼총사 못풀엇음

---
+++ 230119-91.java
@@ -0,0 +1,23 @@
+// 삼총사
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        
+        Arrays.sort(number);
+        
+        // 0을 기준으로 똑같은 숫자가 있는지
+        for(int i = 0; i < number.length; i++) {
+            
+        }
+        
+        
+        // 0을 기준으로 좌측과 우측을 더했을 때 0이 되는지
+        
+        
+        
+        return answer;
+    }
+}

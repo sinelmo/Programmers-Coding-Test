6609e433bf5b5d91252de4c69de7d32ee7f229fd
sinelmo <49057487+sinelmo@users.noreply.github.com>
2023-01-22 23:46:15 +0900

    문자열 내 마음대로 정렬하기 못풀었음

---
+++ 230122-01.java
@@ -0,0 +1,22 @@
+// 문자열 내 마음대로 정렬하기
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        
+        
+        
+        return answer;
+    }
+    
+    public String[] sort(int n, String[] strings) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < strings.length - 1; i++) {
+            if(i == 0) {
+                
+            }
+        }
+        
+        return null;
+    }
+}
